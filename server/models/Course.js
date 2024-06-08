e20c8461443b74d63ee99e63af207c93044a63aa
Mohammed <mohammedhassouna000@gmail.com>
2024-06-08 17:33:53 +0300

    added course schema

---
+++ server/models/Course.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const courseSchema = new mongoose.Schema({
+  course_name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: [String], 
+    default: [],
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+  teacher: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    immutable: true,
+  },
+  deleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+module.exports = Course;
