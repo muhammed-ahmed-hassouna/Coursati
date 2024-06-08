504f4dc49f4ab42b628def2587f9da53bd2ca64f
Mohammed <mohammedhassouna000@gmail.com>
2024-06-08 17:36:24 +0300

    added server

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+const authRoutes = require("./routes/authRoutes")
+const courseRoutes = require("./routes/courseRoutes")
+
+app.use(authRoutes);
+app.use(courseRoutes);
+
+const PORT = process.env.PORT || 3999;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
