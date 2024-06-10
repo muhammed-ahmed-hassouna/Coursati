c31237d38e3e0a5c22ec731ce6839be8487a7fad
Mohammed <mohammedhassouna000@gmail.com>
2024-06-10 07:38:53 +0300

    Add routes to the Index

--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -1,13 +1,31 @@
 import { Routes, Route } from "react-router-dom";
 import ScrollToTop from "../components/ui/ScrollToTop";
+import Login from "./website/login";
+import UnAuthorized from "./website/Unauthorized";
+import NotFound from "./website/NotFound";
+import SignUp from "./website/signup";
+import Home from "./website/Home";
+import AuthProvider from "../providers/AuthProvider";
+import TopNav from "../shared/TopNav";
+import Student from "./website/student";
+import TeacherIndex from "./website/Teacher";
 
 export default function Index() {
   return (
     <>
       <ScrollToTop />
-        <Routes>
-         
-        </Routes>
+      <TopNav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route element={<AuthProvider />}>
+          <Route path="/student" element={<Student />} />
+          <Route path="/teacher" element={<TeacherIndex />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/unauthorized" element={<UnAuthorized />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
